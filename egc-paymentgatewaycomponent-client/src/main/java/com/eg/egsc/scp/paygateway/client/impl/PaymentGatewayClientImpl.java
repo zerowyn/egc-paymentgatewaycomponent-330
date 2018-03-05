9ab42ec483b816d8b3f29bda98b4274977064586
caiqinli <caiqinli@hdsc.com>
2018-03-05 18:12:15 +0800

    [ADD] 添加client接口代码

---
+++ egc-paymentgatewaycomponent-client/src/main/java/com/eg/egsc/scp/paygateway/client/impl/PaymentGatewayClientImpl.java
@@ -0,0 +1,49 @@
+/**
+ * @Probject Name: egc-paymentgatewaycomponent-client
+ * @Path: com.eg.egsc.scp.paygateway.client.implPaymentGatewayClientImpl.java
+ * @Create By caiqinli
+ * @Create In 2018年3月5日 下午6:01:12
+ * TODO
+ */
+package com.eg.egsc.scp.paygateway.client.impl;
+
+import com.eg.egsc.common.exception.CommonException;
+import com.eg.egsc.framework.client.core.BaseApiClient;
+import com.eg.egsc.framework.client.dto.ResponseDto;
+import com.eg.egsc.scp.paygateway.client.PaymentGatewayClient;
+import com.eg.egsc.scp.paygateway.dto.OrderQueryRequestForBackendDto;
+import com.eg.egsc.scp.paygateway.dto.OrderQueryResponseForBackendDto;
+
+
+/**
+ * @Class Name PaymentGatewayClientImpl
+ * @Author caiqinli
+ * @Create In 2018年3月5日
+ */
+public class PaymentGatewayClientImpl extends BaseApiClient implements PaymentGatewayClient {
+  
+  public PaymentGatewayClientImpl(){
+    super();    
+    
+  }
+  
+  /* (non-Javadoc)
+   * @see com.eg.egsc.framework.client.core.BaseApiClient#getContextPath()
+   */
+  @Override
+  protected String getContextPath() {
+    
+    return "/paymentgateway";
+  }
+  
+  
+  public OrderQueryResponseForBackendDto orderQuery(OrderQueryRequestForBackendDto orderQueryRequestForBackendDto) throws CommonException{
+    
+    ResponseDto res = post("/api/pay/orderquery", orderQueryRequestForBackendDto);
+    return (OrderQueryResponseForBackendDto) res.getData();   
+    
+  }
+
+
+
+}
