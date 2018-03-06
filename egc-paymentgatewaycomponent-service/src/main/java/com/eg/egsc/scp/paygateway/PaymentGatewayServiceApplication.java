0ebbf217f83643c812f00e8e6f7609743cce379f
caiqinli <caiqinli@hdsc.com>
2018-03-06 11:41:19 +0800

    [CHG] 开放Euraka网关注册功能

--- egc-paymentgatewaycomponent-service/src/main/java/com/eg/egsc/scp/paygateway/PaymentGatewayServiceApplication.java
+++ egc-paymentgatewaycomponent-service/src/main/java/com/eg/egsc/scp/paygateway/PaymentGatewayServiceApplication.java
@@ -7,14 +7,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 import com.eg.egsc.framework.service.core.ApplicationStarter;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 @ComponentScan(basePackages = {"com.eg.egsc"})
-//@MapperScan(basePackages = {"com.eg.egsc.scp.demo"})
 @MapperScan(basePackages = {"com.eg.egsc.scp.paygateway"})
 public class PaymentGatewayServiceApplication extends SpringBootServletInitializer {
 
