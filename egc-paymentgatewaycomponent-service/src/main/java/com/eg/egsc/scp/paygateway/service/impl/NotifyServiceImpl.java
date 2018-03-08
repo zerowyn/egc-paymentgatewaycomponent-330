03065e86e6b7303bd7718126b6ea7186938caae5
ZhangChenglong <ZhangChenglong@hdsc.com>
2018-03-08 15:38:31 +0800

    [modify]优化代码

--- egc-paymentgatewaycomponent-service/src/main/java/com/eg/egsc/scp/paygateway/service/impl/NotifyServiceImpl.java
+++ egc-paymentgatewaycomponent-service/src/main/java/com/eg/egsc/scp/paygateway/service/impl/NotifyServiceImpl.java
@@ -11,6 +11,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.eg.egsc.egc.paymentbackendsystem.client.PaymentResultInformClient;
 import com.eg.egsc.egc.paymentbackendsystem.dto.ResultInformDto;
 import com.eg.egsc.egc.paymentbackendsystem.dto.ResultInformResponseDto;
+import com.eg.egsc.framework.client.dto.ResponseDto;
 import com.eg.egsc.scp.paygateway.service.NotifyService;
 import com.eg.egsc.scp.paygateway.service.SignatureService;
 import com.eg.egsc.scp.paygateway.service.model.WeiXinNotifyResponse;
@@ -35,82 +36,85 @@ import java.util.Set;
 @Service
 public class NotifyServiceImpl implements NotifyService {
 
-    private final static Logger logger = LoggerFactory.getLogger(NotifyServiceImpl.class);
+  private final static Logger logger = LoggerFactory.getLogger(NotifyServiceImpl.class);
 
-    @Autowired
-    private PaymentResultInformClient paymentResultInformClientImpl;
+  @Autowired
+  private PaymentResultInformClient paymentResultInformClientImpl;
 
-    @Autowired
-    private SignatureService signatureServiceImpl;
+  @Autowired
+  private SignatureService signatureServiceImpl;
 
-    /**
-     * 字符串转换
-     *
-     * @param map
-     * @return
-     */
-    private Map<String, Object> stringNameConversion(Map<String, Object> map) {
-        Set<Map.Entry<String, Object>> entries = map.entrySet();
-        Map<String, Object> newMap = new HashMap<>();
-        for (Map.Entry<String, Object> entry : entries) {
-            String s = StringNameConversionUtils.StringNameConversion(entry.getKey());
-            newMap.put(s, entry.getValue());
-        }
-        return newMap;
+  /**
+   * 字符串转换
+   *
+   * @param map
+   * @return
+   */
+  private Map<String, Object> stringNameConversion(Map<String, Object> map) {
+    Set<Map.Entry<String, Object>> entries = map.entrySet();
+    Map<String, Object> newMap = new HashMap<>();
+    for (Map.Entry<String, Object> entry : entries) {
+      String s = StringNameConversionUtils.StringNameConversion(entry.getKey());
+      newMap.put(s, entry.getValue());
+    }
+    return newMap;
+  }
+
+
+  @Override
+  public String disposeMessage(Map<String, Object> map, Boolean b) {
+    boolean flag;
+    if (b) {
+      // 微信验签
+      logger.info("Starting attestation");
+      flag = signatureServiceImpl.weixinSignatureCheck(map);
+      logger.info("WeChat test result is:{}", flag);
+    } else {
+      // 支付宝验签
+      logger.info("The inspection of alipay.");
+      flag = signatureServiceImpl.alipaySignatureAsyCheck(map);
+      logger.info("The alipay inspection result is:{}", flag);
     }
 
-
-    @Override
-    public String disposeMessage(Map<String, Object> map, Boolean b) {
-        boolean flag;
-        if (b) {
-            // 微信验签
-            logger.info("Starting attestation");
-            flag = signatureServiceImpl.weixinSignatureCheck(map);
-            logger.info("WeChat test result is:{}", flag);
-        } else {
-            //支付宝验签
-            logger.info("The inspection of alipay.");
-            flag = signatureServiceImpl.alipaySignatureAsyCheck(map);
-            logger.info("The alipay inspection result is:{}", flag);
-        }
-
-        // 验签成功后转换字段
-        Map<String, Object> newMap = this.stringNameConversion(map);
-        ResultInformDto resultInformDto = null;
-        if (flag) {
-            String json = JSONObject.toJSONString(newMap);
-            json.replaceAll("\\{", "[");
-            json.replaceAll("}", "]");
-            resultInformDto = JSONObject.parseObject(json, ResultInformDto.class);
-        }
-        // 调用后台接口回传数据
-        ResultInformResponseDto notify = paymentResultInformClientImpl.getNotify(resultInformDto);
-        if (ObjectUtils.isEmpty(notify)) {
-            logger.error("The received message is empty.");
-        }
-        String returnMessage = null;
-        if (b) {
-            //微信返回数据
-            WeiXinNotifyResponse weiXinNotifyResponse = new WeiXinNotifyResponse();
-            if (notify.getReturn_code().equalsIgnoreCase("SUCCESS")) {
-                weiXinNotifyResponse.setReturn_code("SUCCESS");
-                weiXinNotifyResponse.setReturn_msg("OK");
-            } else {
-                weiXinNotifyResponse.setReturn_code("FAIL");
-                weiXinNotifyResponse.setReturn_msg("签名失败");
-            }
-            // 组装返回第三方支付平台的数据
-            returnMessage = ObjecTransformXML.jaxbRequestObjectToXMLForWeiXin(weiXinNotifyResponse);
-
-        } else {
-            //支付宝返回数据
-            if (notify.getReturn_code().equalsIgnoreCase("SUCCESS")) {
-                returnMessage = NotifyConstant.SUCCESS;
-            }
-        }
-        return returnMessage;
+    // 验签成功后转换字段
+    Map<String, Object> newMap = this.stringNameConversion(map);
+    ResultInformDto resultInformDto = null;
+    if (flag) {
+      String json = JSONObject.toJSONString(newMap);
+      json.replaceAll("\\{", "[");
+      json.replaceAll("}", "]");
+      resultInformDto = JSONObject.parseObject(json, ResultInformDto.class);
     }
+    // 调用后台接口回传数据
+    ResponseDto dto = paymentResultInformClientImpl.getNotify(resultInformDto);
+
+    if (!(ObjectUtils.isEmpty(dto) && ObjectUtils.isEmpty(dto.getData()))) {
+      logger.error("The received message is erro.");
+    }
+
+    ResultInformResponseDto notify = JSONObject.parseObject(JSONObject.toJSONString(dto.getData()), ResultInformResponseDto.class);
+    String returnMessage = null;
+    if (b) {
+      // 微信返回数据
+      WeiXinNotifyResponse weiXinNotifyResponse = new WeiXinNotifyResponse();
+      if (notify.getReturnCode().equalsIgnoreCase("SUCCESS")) {
+        weiXinNotifyResponse.setReturn_code("SUCCESS");
+        weiXinNotifyResponse.setReturn_msg("OK");
+      } else {
+        weiXinNotifyResponse.setReturn_code("FAIL");
+        weiXinNotifyResponse.setReturn_msg("签名失败");
+      }
+      // 组装返回第三方支付平台的数据
+      returnMessage = ObjecTransformXML.jaxbRequestObjectToXMLForWeiXin(weiXinNotifyResponse);
+
+    } else {
+      // 支付宝返回数据
+      if (notify.getReturnCode().equalsIgnoreCase("SUCCESS")) {
+        returnMessage = NotifyConstant.SUCCESS;
+      }
+    }
+    return returnMessage;
+  }
 }
 
 
