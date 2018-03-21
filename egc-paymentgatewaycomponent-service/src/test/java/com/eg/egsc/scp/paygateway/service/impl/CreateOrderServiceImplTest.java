4e963f02da57e286bd4292e20559d36c61d53634
lihui <lihui@hdsc.com>
2018-03-21 10:08:31 +0800

    下单功能-打开eureka注解

--- egc-paymentgatewaycomponent-service/src/test/java/com/eg/egsc/scp/paygateway/service/impl/CreateOrderServiceImplTest.java
+++ egc-paymentgatewaycomponent-service/src/test/java/com/eg/egsc/scp/paygateway/service/impl/CreateOrderServiceImplTest.java
@@ -6,23 +6,13 @@ package com.eg.egsc.scp.paygateway.service.impl;
 import com.eg.egsc.scp.paygateway.PaymentGatewayServiceApplication;
 import com.eg.egsc.scp.paygateway.dto.RequestForGetOpenIdDto;
 import com.eg.egsc.scp.paygateway.service.CreateOrderService;
-import org.apache.commons.fileupload.servlet.ServletRequestContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.support.ServletContextApplicationContextInitializer;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.bind.annotation.RequestHeader;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletRequest;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author lihui
