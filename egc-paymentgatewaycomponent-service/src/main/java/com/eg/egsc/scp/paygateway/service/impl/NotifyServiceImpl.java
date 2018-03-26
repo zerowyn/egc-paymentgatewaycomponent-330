e45f95974cb20d73fb972192f1a8d6fb611e695c
fandong <fandong@hdsc.com>
2018-03-26 09:19:21 +0800

    [add]添加notifyServiceImpl的日志

--- egc-paymentgatewaycomponent-service/src/main/java/com/eg/egsc/scp/paygateway/service/impl/NotifyServiceImpl.java
+++ egc-paymentgatewaycomponent-service/src/main/java/com/eg/egsc/scp/paygateway/service/impl/NotifyServiceImpl.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Set;
 
 import com.alibaba.fastjson.JSON;
+import com.eg.egsc.scp.paygateway.mapper.CodeMapsMapper;
 import com.eg.egsc.scp.paygateway.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -84,7 +85,7 @@ public class NotifyServiceImpl implements NotifyService {
         Map<String, Object> newMap = this.stringNameConversion(map);
         ResultInformDto resultInformDto = null;
         if (!flag) {
-            return "";
+            return "Attestation of failure";
         }
         if (sign) {
             String json = JSONObject.toJSONString(newMap).replaceAll("\\[", "{").replaceAll("]", "}");
@@ -103,7 +104,7 @@ public class NotifyServiceImpl implements NotifyService {
         ResponseDto dto = paymentResultInformClientImpl.getNotify(resultInformDto);
         if (ObjectUtils.isEmpty(dto) || ObjectUtils.isEmpty(dto.getData())) {
             logger.error("The received message is erro.");
-            return "";
+            return "The message received was empty.";
         }
         ResultInformResponseDto notify = JSONObject.parseObject(JSONObject.toJSONString(dto.getData()), ResultInformResponseDto.class);
         String returnMessage = "";
